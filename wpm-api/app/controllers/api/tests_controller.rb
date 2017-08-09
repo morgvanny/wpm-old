0f5626b3532ad8484375cae442fac40b26e94387
morgvanny <morgvanny@gmail.com>
2017-08-09 15:52:48 -0600

    set up tests_controller.rb

---
+++ wpm-api/app/controllers/api/tests_controller.rb
@@ -0,0 +1,24 @@
+class Api::TestsController < ApplicationController
+
+  def index
+    render json: Test.all
+  end
+
+  def create
+    test = Test.new(test_params)
+    if test.save
+      render json: test
+    else
+      render json: { message: test.errors }, status: 400
+    end
+
+  def show
+    render Test.find_by(id: params[:id])
+  end
+
+  private
+
+    def test_params
+      params.require(:test).permit(:team, :wpm, :length)
+    end
+end
