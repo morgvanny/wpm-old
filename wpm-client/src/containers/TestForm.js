1704435c8b4fb0a99897a7345d09a828e980f3a8
morgvanny <morgvanny@gmail.com>
2017-08-25 14:07:36 -0600

    choose team by clicking, and record time when clicked on TestForm.js container

--- wpm-client/src/containers/TestForm.js
+++ wpm-client/src/containers/TestForm.js
@@ -5,6 +5,14 @@ import { createTest } from '../actions/tests';
 
 class TestForm extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      date: undefined
+    };
+  }
+
   handleOnChange = event => {
     const { name, value } = event.target;
     const currentTestFormData = Object.assign({}, this.props.testFormData, {
@@ -18,21 +26,37 @@ class TestForm extends Component {
     this.props.createTest(this.props.testFormData)
   }
 
+  startTest = text => {
+    console.log(text)
+    this.props.testFormData.team = text
+    this.setState({
+      date: new Date(Date.now())
+    })
+    console.log(this.props.testFormData)
+    console.log(this.state.date)
+  }
 
   render() {
     const { team, wpm, length } = this.props.testFormData;
 
     return (
       <div>
-        Take the test
+        <h1>Choose your team, to start the test!</h1>
+        <div>
+          <button 
+            onClick={this.startTest.bind(this, "Red")}
+            id="Red"
+          >
+            Red
+          </button>
+          <button 
+            onClick={this.startTest.bind(this, "Blue")}
+            id="Blue"
+          >
+            Blue
+          </button>
+        </div>
         <form onSubmit={this.handleOnSubmit}>
-          <label htmlFor="team">Team:</label>
-          <input
-            type="text"
-            onChange={this.handleOnChange}
-            name="team"
-            value={team}
-          />
           <label htmlFor="wpm"> wpm:</label>
           <input
             type="number"
