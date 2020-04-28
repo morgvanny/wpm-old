ec2f20261dbe031f31a168086c21cf388127dcb1
Morgan VanYperen <morgvanny@gmail.com>
2020-04-27 21:22:00 -0700

    add users

--- wpm-api/config/routes.rb
+++ wpm-api/config/routes.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
-    resources :tests, except: [:new, :edit]
+    resources :users
+    resources :sessions, only: %i[new create destroy]
+    resources :tests, except: %i[new edit]
   end
 end
